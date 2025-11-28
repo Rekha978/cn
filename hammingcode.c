a265ec9821590532e05c0c649de9ea45e35c09a8
Rekha978 <152130211+Rekha978@users.noreply.github.com>
2025-11-28 06:37:42 +0530

    Update hammingcode.c

--- hammingcode.c
+++ hammingcode.c
@@ -1,86 +1,26 @@
-
 #include <stdio.h>
-#include <math.h>
-int input[32];
-int code[32];
-int ham_calc(int,int);
-void main()
-{
-	int n,i,p_n = 0,c_l,j,k;
-	printf("Please enter the length of the Data Word: ");
-	scanf("%d",&n);
-	printf("Please enter the Data Word:\n");
-	for(i=0;i<n;i++)
-	{
-		scanf("%d",&input[i]);
-	}
-
-	i=0;
-	while(n>(int)pow(2,i)-(i+1))
-	{
-		p_n++;
-		i++;
-	}
-		
-	c_l = p_n + n;
-
-	j=k=0;
-	for(i=0;i<c_l;i++)
-	{
-		
-		if(i==((int)pow(2,k)-1))
-		{
-			code[i]=0;
-			k++;
-		}
-		else
-		{
-			code[i]=input[j];
-			j++;
-		}
-	}
-	for(i=0;i<p_n;i++)
-	{
-		int position = (int)pow(2,i);
-		int value = ham_calc(position,c_l);
-		code[position-1]=value;
-	}
-	printf("\nThe calculated Code Word is: ");
-	for(i=0;i<c_l;i++)
-		printf("%d",code[i]);
-	printf("\n");
-	printf("Please enter the received Code Word:\n");
-	for(i=0;i<c_l;i++)
-		scanf("%d",&code[i]);
-
-	int error_pos = 0;
-	for(i=0;i<p_n;i++)
-	{
-		int position = (int)pow(2,i);
-		int value = ham_calc(position,c_l);
-		if(value != 0)
-			error_pos+=position;
-	}
-	if(error_pos == 1)
-		printf("The received Code Word is correct.\n");
-	else
-		printf("Error at bit position: %d\n",error_pos);
-}
-int ham_calc(int position,int c_l)
-{
-	int count=0,i,j;
-	i=position-1;
-	while(i<c_l)
-	{
-		for(j=i;j<i+position;j++)
-		{
-			if(code[j] == 1)
-				count++;
-		}
-		i=i+2*position;
-	}
-	if(count%2 == 0)
-		return 0;
-	else
-		return 1;
+int main() {
+    int d[10], r[10], p1, p2, p4, p, i;
+    printf("Enter 4-bit data: ");
+    for (i = 3; i >= 0; i--)
+        scanf("%d", &d[i]);
+    p1 = d[0] ^ d[1] ^ d[3];
+    p2 = d[0] ^ d[2] ^ d[3];
+    p4 = d[1] ^ d[2] ^ d[3];
+    printf("Transmitted: %d%d%d%d%d%d%d\n", p1, p2, d[0], p4, d[1], d[2], d[3]);
+    printf("Enter received: ");
+    for (i = 6; i >= 0; i--)
+        scanf("%d", &r[i]);
+    p1 = r[6] ^ r[5] ^ r[3] ^ r[1];
+    p2 = r[6] ^ r[4] ^ r[3] ^ r[0];
+    p4 = r[5] ^ r[4] ^ r[3] ^ r[2];
+    p = p4 * 4 + p2 * 2 + p1;
+    if (p) {
+        printf("Error at: %d\nCorrected: ", p);
+        r[7 - p] ^= 1;
+        for (i = 6; i >= 0; i--)
+            printf("%d", r[i]);
+    } else
+        printf("No error\n");
+    return 0;
 }
